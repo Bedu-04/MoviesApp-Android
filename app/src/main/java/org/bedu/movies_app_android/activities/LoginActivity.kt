28a970a86b956b0237592f7e728b3d92fad1de8d
Kiwicode99 <gkg28@hotmail.com>
2023-06-13 17:41:12 -0600

    Se agrego login y signup activity

---
+++ app/src/main/java/org/bedu/movies_app_android/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.bedu.movies_app_android.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.bedu.movies_app_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
