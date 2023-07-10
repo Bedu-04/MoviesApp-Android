65e3b938e31f2781e57954f4e9b08fa0f70a7a54
kewyn <gkg28@hotmail.com>
2023-07-09 20:32:37 -0600

    Se agregó opcion de google sign in

--- app/src/main/java/org/bedu/movies_app_android/ui/activities/LoginActivity.kt
+++ app/src/main/java/org/bedu/movies_app_android/ui/activities/LoginActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import org.bedu.movies_app_android.R
 import org.bedu.movies_app_android.databinding.ActivityLoginBinding
 
@@ -12,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val GOOGLE_SIGN_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +42,57 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             } else{
-                Toast.makeText(this, "Fields cannnot be empty", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
             }
         }
+
+        //Google Sign In Button
+        binding.google.setOnClickListener {
+            //Configuration
+            val googleConfig = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConfig)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+
         binding.signupRedirectText.setOnClickListener {
             val signupIntent = Intent(this, SignupActivity::class.java)
             startActivity(signupIntent)
         }
+}
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if(requestCode == GOOGLE_SIGN_IN){
+
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+        try {
+            val account = task.getResult(ApiException::class.java)
+
+            if(account != null){
+
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } catch (e: ApiException){
+            Toast.makeText(this, "Failed to Authenticate", Toast.LENGTH_SHORT).show()
+        }
     }
+}
 }
